239c7f80d0b977a2c4e26472b9d2c1ad89c92c44
Anik Patra <jucanik@amazon.com>
2021-09-11 16:23:29 +0530

    Build PPMTool

---
+++ src/test/java/io/agileintelligence/todoapp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
